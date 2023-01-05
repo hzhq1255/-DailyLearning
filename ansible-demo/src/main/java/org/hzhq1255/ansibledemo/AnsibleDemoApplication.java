2fcd4d6685e271bb0293d327c1e769d03aedceb6
hzhq1255 <hzhq1255@163.com>
2023-01-05 16:44:30 +0800

    update ansible demo

---
+++ ansible-demo/src/main/java/org/hzhq1255/ansibledemo/AnsibleDemoApplication.java
@@ -0,0 +1,13 @@
+package org.hzhq1255.ansibledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnsibleDemoApplication.class, args);
+    }
+
+}
---
+++ ansible-demo/src/test/java/org/hzhq1255/ansibledemo/AnsibleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hzhq1255.ansibledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
